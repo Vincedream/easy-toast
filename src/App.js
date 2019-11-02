4628178c6d138ef23d05a576764dd5f689e9b3c4
Vincedream <callmevincehua@gmail.com>
2019-11-02 16:26:14 +0800

    feat: finish normal toast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        isToastShow: false, // 是否展示 Toast
+        toastText: '', // Toast 文字内容
+    }
+
+    // 设置 Toast 属性
+    handleToastShow = (toastText, showTime) => {
+        this.setState({
+            isToastShow: true,
+            toastText
+        });
+        // 定时销毁 Toast Dom
+        setTimeout(() => {
+            this.setState({
+                isToastShow: false
+            })
+        }, showTime)
+    }
+
+    // 显示 Toast
+    handleShowToast = () => {
+        this.handleToastShow('this is a toast', 3000)
+    }
+
+    render() {
+        const { isToastShow, toastText } = this.state;
+        return (
+            <div>
+                <button onClick={this.handleShowToast}>show toast</button>
+                {isToastShow && <div className="toast-wrap">
+                    <div className="toast-mask" />
+                    <div className="toast-text">{toastText}</div>
+                </div>}
+            </div>
+        )
+    }
 }
 
 export default App;
